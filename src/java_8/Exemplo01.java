96f4dc9308c2924ce81885300dfc0f297dacb701
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-10-31 18:20:51 -0300

    criando Thread com e sem Runnable
    comparativo entre Java 8 e versoes anteriores

---
+++ src/java_8/Exemplo01.java
@@ -0,0 +1,22 @@
+package java_8;
+
+public class Exemplo01 {
+
+    public static void main(String[] args) {
+        // Forma tradicional de implementar usando Runnable sem Expressões Lambda
+        Thread thread1 = new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("Thread tradicional com Runnable sem expressão lambda");
+                    }
+                }
+        );
+        thread1.start();
+
+        // Forma recomendada a partir do Java 8 (Veja ExecutorsService)
+        Thread thread2 = new Thread(() -> System.out.println("Thread usando Runnable em uma expressão lambda"));
+        thread2.start();
+    }
+
+}
