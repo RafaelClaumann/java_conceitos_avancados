c5d5e97dfb9347617ad343a416d825cce7c95fe6
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2024-10-31 18:27:41 -0300

    criando uma @FunctionalInterface e usando lambda sem parametros

---
+++ src/java_8/Exemplo02.java
@@ -0,0 +1,28 @@
+package java_8;
+
+// Lembrando que 'void desenha();' é 'public abstract void desenha();'
+// A anotação @FunctionalInterface é opcional, mas serve como uma segurança indicando que a interface não mudará
+@FunctionalInterface
+interface Figura {
+    void desenha();
+}
+
+public class Exemplo02 {
+
+    public static void main(String[] args) {
+        // Desenhando Figura sem usar lambda expressions
+        Figura figura1 = new Figura() {
+            @Override
+            public void desenha() {
+                System.out.println("Criando Figura sem expressão lambda mesmo sabendo que Figura é uma @FunctionalInterface");
+            }
+        };
+        figura1.desenha();
+
+        // Desenhando Figura usando lambda expressions
+        // Implementação de expressão lambda sem argumentos
+        Figura figura2 = () -> System.out.println("Criando Figura usando expressão lambda");
+        figura2.desenha();
+    }
+
+}
